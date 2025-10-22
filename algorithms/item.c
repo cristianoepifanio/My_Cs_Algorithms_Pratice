3cab21f67fd445213d23388973d523c85ad0d9cb
Cristiano Epifânio <106474458+cristianoepifanio@users.noreply.github.com>
2025-10-22 21:58:52 +0300

    Update item.c

---
+++ algorithms/item.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    /* code */
+    int a = 5;
+    int b = 4;
+    int c = a - b;
+    printf("%d + %d = %d\n",a,b,a+b);
+    printf("%d - %d = %d\n",a,b,c);
+    printf("%d x %d = %d\n",a,b,a*b);
+    return 0;
+}
