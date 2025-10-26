b46c342e97383abe39dd803b65f2e2a99b6f93f0
Cristiano Epifânio <106474458+cristianoepifanio@users.noreply.github.com>
2025-10-26 14:10:17 +0200

    -

--- algorithms/item.c
+++ algorithms/item.c
@@ -3,10 +3,10 @@
 int main()
 {
     /* code */
-    int a = 6;
+    int a = 3;
     int b = 4;
     int c = a - b;
-    printf("%d + %d = %d\n",a,b,a+b);
+    printf("%d + %d = %d\n",a,b,a+b);   
     printf("%d - %d = %d\n",a,b,c);
     printf("%d x %d = %d\n",a,b,a*b);
     return 0;
--- algorithms/teste.c
+++ algorithms/teste.c
@@ -2,7 +2,16 @@
 
 int main()
 {
-    printf("É só um teste de novo");
-    return 0;
+    printf("É só um teste de novo\n");
+
+    /* Aqui começa meus testes*/
+   char name[100];
+
+   printf("Enter a name:\n");
+   fgets(name, sizeof(name), stdin);
+
+   printf("You entered: \n");
+   printf("%s", name);
+
 
 }
