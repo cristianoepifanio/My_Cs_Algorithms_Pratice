b29de5f096068029684c9edc9bbdf0f2bf3c1481
Cristiano Epifânio <106474458+cristianoepifanio@users.noreply.github.com>
2025-12-06 15:57:39 +0200

    -

--- algorithms/EpifanioLopesJuniorCristiano/Main.c
+++ algorithms/EpifanioLopesJuniorCristiano/Main.c
@@ -1,39 +1,113 @@
-#include<stdio.h>
-int funcOne(int a){
-    printf("Function One: %d\n",a);
-    return a;
-}
-int funcTwo(int b){
-    printf("Function Two: %d\n",b);
-    return b;
-}
-int funcThree(int c){
-    printf("Function Three: %d\n",c);
-    return c;
-}
+#include <stdio.h>
 
-int main(){
+void escalar_product_v3(void) {
+    int x1, x2, y1, y2, z1, z2;
+    printf("Enter coordinates of first vector (x1 y1 z1): ");
+    scanf("%d %d %d", &x1, &y1, &z1);
+    printf("Enter coordinates of second vector (x2 y2 z2): ");
+    scanf("%d %d %d", &x2, &y2, &z2);
+    int scalar_product = x1 * x2 + y1 * y2 + z1 * z2;
+    printf("\n");
+    printf("Scalar product: %d\n", scalar_product);
+    printf("\n");
+};
+
+void escalar_product_n(void) {
+    /* Scala product of 2 n-dimensional vectors */  
+     int n, i;
+    int *A, *B; 
+    int scalar_product = 0;
+    printf("Enter the dimension of the vectors: ");
+    scanf("%d", &n);    
+    A = (int*) malloc(n * sizeof(int));
+    B = (int*) malloc(n * sizeof(int));
+    printf("Enter the %d elements of vector A:\n",n);
+    for(i = 0; i < n; i++) {
+        printf("Type the %dth cordinate: ", i+1);
+        scanf("%d", &A[i]);
+    }
+    printf("Enter the %d elements of vector B:\n",n);
+    for(i = 0; i < n; i++) {
+        printf("Type the %dth cordinate: ", i+1);
+        scanf("%d", &B[i]);
+    }
+    for(i = 0; i < n; i++) {
+        scalar_product += A[i] * B[i];
+    }
+    printf("\n");
+    printf("The scalar product of the vectors is: %d\n", scalar_product);
+    printf("\n");
+    free(A);
+    free(B);
+};
+
+    void subtraction_Vn(void) {
+         /* Subtraction of 2 n-dimensional vectors */
+    int n, i;
+    int *A, *B, *C; 
+    printf("Enter the dimension of the vectors: ");
+    scanf("%d", &n);    
+    A = (int*) malloc(n * sizeof(int));
+    B = (int*) malloc(n * sizeof(int));
+    C = (int*) malloc(n * sizeof(int));
+    printf("Enter the %d elements of vector A:\n",n);
+    for(i = 0; i < n; i++) {
+        printf("Type the %dth cordinate: ", i+1);
+        scanf("%d", &A[i]);
+    }
+    printf("Enter the %d elements of vector B:\n",n);
+    for(i = 0; i < n; i++) {
+        printf("Type the %dth cordinate: ", i+1);
+        scanf("%d", &B[i]);
+    }
+    for(i = 0; i < n; i++) {
+        C[i] = A[i] - B[i];
+    }
+    printf("\n");
+    printf("The subtraction of the vectors (A - B) is: (");   
+    for(i = 0; i < n; i++) {
+        if(i != n-1) {
+            printf("%d, ", C[i]);
+        } else {
+            printf("%d)\n", C[i]);
+        }
+    }
+    printf("\n");
+    free(A);
+    free(B);
+    free(C);
+    };
+
+int main()
+{
     int opt;
-        printf("Select an option:\n1. Option One\n2. Option Two\n3. Option Three\n0. Exit\n");
-        scanf("%d",&opt);
-        do
+    printf("Select an option:\n1. Option One\n2. Option Two\n3. Option Three\n0. Exit\n");
+    scanf("%d", &opt);
+    do
+    {
+        switch (opt)
         {
-           switch (opt)
-           {
-           case 1:
-            funcOne(10);
+        case 1:
+            escalar_product_v3();
             break;
-            case 2:
-            funcTwo(20);    
+        case 2:
+
+        break;
+            escalar_product_n();
             break;
-            case 3:
-            funcThree(30);
-            break;
+        case 3:
+            subtraction_Vn();
            
-           default:
             break;
-           }
-        } while (opt!=0);
+
+        default:
+            break;
+        }
+        printf("Select an option:\n1. Option One\n2. Option Two\n3. Option Three\n0. Exit\n");
+        scanf("%d", &opt);
+    } while (opt != 0);
+
+
+
     return 0;
-        
 }
